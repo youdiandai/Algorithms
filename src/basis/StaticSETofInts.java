24643e5acd518df8380e3354a222fa657caed47b
ChangXin <1561753947@qq.com>
2018-06-05 22:23:13 +0800

    二分查找

---
+++ src/basis/StaticSETofInts.java
@@ -0,0 +1,33 @@
+package basis;
+import java.util.Arrays;
+
+public class StaticSETofInts {
+    private int[] a;
+    public StaticSETofInts(int[] keys)
+    {
+        a=new int[keys.length];
+        for(int i=0;i<keys.length;i++)
+        {
+            a[i]=keys[i];
+        }
+        Arrays.sort(a);
+
+    }
+    private int rank(int key)
+    {
+        int lo = 0;
+        int hi = a.length-1;
+        while (lo<hi)
+        {
+            int mid = lo+(hi-lo)/2;
+            if(k<a[mid]) hi=mid-1;
+            else if (key>a[mid]) lo=mid+1;
+            else return mid;
+        }
+        return -1;
+    }
+    public boolean contains(int keys)
+    {
+        return rank(keys) != -1;
+    }
+}
