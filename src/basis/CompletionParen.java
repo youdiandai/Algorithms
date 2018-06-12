91358eec5791101fcfd4df743719b9070d577566
ChangXin <1561753947@qq.com>
2018-06-13 01:18:03 +0800

    补全括号

--- src/basis/CompletionParen.java
+++ src/basis/CompletionParen.java
@@ -3,44 +3,46 @@ package basis;
 import edu.princeton.cs.algs4.StdOut;
 
 public class CompletionParen {
-    private static final char LEFT_PAREN     = '(';
-    private static final char RIGHT_PAREN    = ')';
+    private static final String LEFT_PAREN     = "(";
+    private static final String RIGHT_PAREN    = ")";
     public static String completion(String s)
     {
-        LinkListQueue<Character> a = new LinkListQueue<Character>();
-        LinkListQueue<Character> b = new LinkListQueue<Character>();
-        LinkListQueue<Character> c = new LinkListQueue<Character>();
-        for(int i=0;i<s.length();i++)
+        String[] ss = s.split(" ");
+        for(int i=0;i<ss.length;i++)
         {
-            a.enqueue(s.charAt(i));
+            StdOut.print(ss[i]);
         }
-        while (!a.isEmpty())
+        StdOut.println();
+        LinkListStack<String> val = new LinkListStack<String>();
+        LinkListStack<String> opt = new LinkListStack<String>();
+        for(int i=0;i<ss.length;i++)
         {
-            char x = a.dequeue();
-            b.enqueue(x);
-            if(x==RIGHT_PAREN)
+            if(!ss[i].equals(RIGHT_PAREN))
             {
-                c.enqueue(LEFT_PAREN);
-                while (!b.isEmpty())
+                if(ss[i].equals("+")||ss[i].equals("-")||ss[i].equals("*")||ss[i].equals("/"))
                 {
-                    c.enqueue(b.dequeue());
+                    opt.push(ss[i]);
                 }
-                while (!c.isEmpty())
+                else
                 {
-                    b.enqueue(c.dequeue());
+                    val.push(ss[i]);
                 }
             }
+            else
+            {
+                String temps=RIGHT_PAREN;
+                temps=val.pop()+temps;
+                temps=opt.pop()+temps;
+                temps=val.pop()+temps;
+                temps=LEFT_PAREN+temps;
+                val.push(temps);
+            }
         }
-        String sum="";
-        for (Character x:b)
-        {
-            sum+=x;
-        }
-        return sum;
+        return val.pop();
     }
     public static void main(String[] args)
     {
-        String s = "1+2)*3-4)*5-6)))";
+        String s = "1 + 2 ) * 3 - 4 ) * 5 - 6 ) ) )";
         StdOut.println(completion(s));
     }
 }
